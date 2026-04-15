391b3c5ed6ed017679a158307080a29fe7bcdb53
wiros <sl6277790@gmail.com>
2026-04-15 18:40:00 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+
+if command -v apt-get >/dev/null 2>&1; then
+    echo "Installing for ALTLinux..."
+    sudo apt-get install swayfx rofi mako fastfetch waybar swaybg hyprshot kitty thunar fonts-ttf-jetbrains-mono-nl fonts-font-awesome cliphist wlogout swaylock
+    
+
+elif command -v pacman >/dev/null 2>&1; then
+    echo "Installing for Arch..."
+    yay -S swayfx rofi mako fastfetch waybar swaybg hyprshot kitty thunar ttf-jetbrains-mono otf-font-awesome cliphist wiremix wlogout bluetui swaylock-effects    
+
+else
+    echo "Unsupported distro"
+    exit 1
+fi
+
+cp -r * ~/.config
+
+echo "Done!"
