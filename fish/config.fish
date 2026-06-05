a8d7308966112ed08a0c6359ce48c3f2a93ff20e
wiros <sl6277790@gmail.com>
2026-06-05 15:39:13 +0300

    Add fish config

---
+++ fish/config.fish
@@ -0,0 +1,34 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+# Created by `pipx` on 2026-02-18 14:25:44
+set PATH $PATH /home/wiros/.local/bin
+
+set --global fish_color_autosuggestion 77a2c7
+set --global fish_color_cancel --reverse
+set --global fish_color_command 419be8
+set --global fish_color_comment red
+set --global fish_color_cwd green
+set --global fish_color_cwd_root red
+set --global fish_color_end green
+set --global fish_color_error brred
+set --global fish_color_escape brcyan
+set --global fish_color_history_current --bold
+set --global fish_color_host normal
+set --global fish_color_host_remote yellow
+set --global fish_color_normal normal
+set --global fish_color_operator brcyan
+set --global fish_color_param cyan
+set --global fish_color_quote yellow
+set --global fish_color_redirection cyan --bold
+set --global fish_color_search_match --background=111
+set --global fish_color_selection white --bold --background=brblack
+set --global fish_color_status red
+set --global fish_color_user brgreen
+set --global fish_color_valid_path --underline=single
+set --global fish_pager_color_completion normal
+set --global fish_pager_color_description B3A06D --italics
+set --global fish_pager_color_prefix cyan --bold --underline=single
+set --global fish_pager_color_progress brwhite --background=cyan
+set --global fish_pager_color_selected_background --reverse
